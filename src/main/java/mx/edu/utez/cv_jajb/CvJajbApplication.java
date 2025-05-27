9412064f7d3876a8de251a97ab30b3f067ce5335
JaredJuarez <jaredjuarez46@gmail.com>
2025-05-27 15:52:37 -0600

    primer  commit

---
+++ src/main/java/mx/edu/utez/cv_jajb/CvJajbApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_jajb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvJajbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvJajbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cv_jajb/CvJajbApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_jajb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvJajbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
